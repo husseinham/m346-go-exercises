3746b644737f94f9edfafe384fd2e25e68e4f960
araberhus <hammadepes@gmail.com>
2024-12-20 14:46:01 +0100

    Anpassung Lösung GO1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,10 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
-	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
-	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
-	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
-	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
-	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
+	// TODO: Variablen deklarieren und initialisieren
+	var firstName string = "Max"
+	var lastName string = "Muster"
+	var age int = 25
+	var dayOfBirth int = 15
+	var monthOfBirth string = "Mai"
+	var yearOfBirth int = 1998
+	var zodiacSign rune = '\u264A' // Beispiel: Waage (Unicode: ♊)
+	var hobbies []string = []string{"Programmieren", "Lesen", "Wandern"}
+	var siblings int = 2
+	var favoriteColor string = "Blau"
+
+	// Ausgabe
+	fmt.Println("Steckbrief")
+	fmt.Println("Vorname:", firstName)
+	fmt.Println("Nachname:", lastName)
+	fmt.Println("Alter:", age)
+	fmt.Println("Geburtstag:", dayOfBirth, monthOfBirth, yearOfBirth)
+	fmt.Println("Sternzeichen:", string(zodiacSign))
+	fmt.Println("Hobbies:", hobbies)
+	fmt.Println("Anzahl Geschwister:", siblings)
+	fmt.Println("Lieblingsfarbe:", favoriteColor)
+
+	// TODO: Anzahl der Geschwister um 1 erhöhen
+	siblings++
+	fmt.Println("Neue Anzahl Geschwister:", siblings)
 }
